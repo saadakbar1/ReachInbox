04d6072db4f1b7f39a47b1ab85a20f091dcdddc3
Saad Akbar <102979976+saadakbar1@users.noreply.github.com>
2024-08-11 10:14:45 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
 
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/"
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-inbox-umber.vercel.app/"
   };
 
   return (
